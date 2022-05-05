3aa8edd5a9ad20e3bb7826baf4f5e74c8b01c738
Bley <bleygui@gmail.com>
2022-05-05 01:03:16 -0300

    Atualização pagína em tempo de desenvolvimento

--- Program.cs
+++ Program.cs
@@ -5,7 +5,7 @@ using Microsoft.Extensions.Logging;
 using System;
 using System.Collections.Generic;
 using System.Linq;
-using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc.Razor.RuntimeCompilation;
 
 namespace WebSitePortifolio
 {
