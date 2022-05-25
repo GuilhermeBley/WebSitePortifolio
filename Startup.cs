5a89288a8dfbd43ba79c0db1901658fdf6733f5e
Bley <bleygui@gmail.com>
2022-05-24 22:31:27 -0300

    BackGround, Nav e inicio corpo.

--- Startup.cs
+++ Startup.cs
@@ -24,6 +24,7 @@ namespace WebSitePortifolio
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddRazorPages();
+            services.AddServerSideBlazor();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
